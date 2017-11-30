5cc8ba5b4e4975f41acc8cea1333acac7a1b7571
appreciated <goebeljohannes@gmx.net>
2017-11-30 15:51:08 +0100

    Update electron-package.sh

--- electron-package.sh
+++ electron-package.sh
@@ -1,8 +1,12 @@
-npm list --depth 1 --global electron-packager
-sudo npm install electron-packager -g
-rm -r -f ./build/electron-vaadin-darwin-x64/
+echo "Check if electron-packager is installed"
+npm list --depth 1 --global electron-packager > /dev/null 2>&1 || sudo npm install electron-packager -g
+echo "Removing old build"
+rm -r -f ./build/
+mkdir -p ./build/
 cd electron-src
+echo "Installing node dependencies"
 npm install
+echo "Copying Jar"
 mkdir -p ./electron-vaadin/
 cp ../jar/*.jar ./electron-vaadin/
 electron-packager . --icon=icon.ico  --ignore=README.md --ignore=.npmignore --ignore=.travis.yml --out ../build/
