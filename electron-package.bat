add605152994679167ab05b367c103c738b89357
appreciated <goebeljohannes@gmx.net>
2017-11-30 15:14:00 +0100

    Update electron-package.bat

--- electron-package.bat
+++ electron-package.bat
@@ -1,21 +1,16 @@
-call npm install electron-packager -g
+:: Check if the electron packager is already installed. If not -> install it
+call npm list --depth 1 --global electron-packager
+if errorlevel 1 npm install electron-packager -g
 
 :: Clean up build dir
-rmdir output\electron-vaadin-win32-x64 /s /q  2>nul
+md build\ 2> nul
+rmdir build\electron-vaadin-win32-x64 /s /q  2>nul
 rmdir electron-src\electron-vaadin-win32-x64 /s /q  2>nul
 
 cd electron-src
-
+xcopy ..\jar .\electron-vaadin\ /E /Y
 :: Install node dependencies
 call npm install
 :: Package electron
-call electron-packager . --icon=icon.ico --ignore=README.md --ignore=.npmignore --ignore=.travis.yml
-:: Package Java Application
-xcopy ..\jar .\electron-vaadin-win32-x64\electron-vaadin\ /E /Y
-
-cd ..
-
-:: Copy the built electron application to an independent dir
-md output\ 2> nul
-move electron-src\electron-vaadin-win32-x64 output\
+call electron-packager . --icon=icon.ico --ignore=README.md --ignore=.npmignore --ignore=.travis.yml --out ../build/
 PAUSE
